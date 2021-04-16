f873efc9406dd85094a345110f1492808623e37b
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2021-04-16 10:45:14 +0300

    Add component ConstructorPage and Container

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import Navigation from "../Navigation";
-
+import Container from '../Container';
 // import routes from "../../routes";
 
 import styles from "./AppBar.module.scss";
@@ -12,12 +12,13 @@ import logo from "./Logo.png";
 
 const AppBar = () => {
   return (
+    <Container>
     <header>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <NavLink to="/" className={styles.logoLink}>
-            <img src={logo} alt="" width="50px" height="50px" />
-          </NavLink>
+        <NavLink to="/" className={styles.logoLink}>
+          <img src={logo} alt="" width="50px" height="50px" />
+        </NavLink>
         </div>
 
         <Navigation />
@@ -27,9 +28,11 @@ const AppBar = () => {
           width="50px"
           height="50px"
           className={styles.ava}
-        />
+          />
+        
       </div>
     </header>
+  </Container>
   );
 };
 
