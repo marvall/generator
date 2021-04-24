c88a0fca168bd63887f59efcec47e3e5db64fc42
Yevheniia Petrychenko <jane2509@gmail.com>
2021-04-24 23:44:06 +0100

    adding dev files on movies and sites carousel

--- src/components/pages/homePage/WebSites/WebSites.jsx
+++ src/components/pages/homePage/WebSites/WebSites.jsx
@@ -1,14 +1,49 @@
-import Carousel from "react-multi-carousel";
+import Carousel from 'react-multi-carousel';
+import WebSiteItem from './WebSiteItem/WebSiteItem';
 
-import "react-multi-carousel/lib/styles.css";
-import Style from "./WebSites.module.scss";
+import 'react-multi-carousel/lib/styles.css';
+import Style from './WebSites.module.scss';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1280 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
 
 const WebSite = () => {
   return (
-    <section className={Style.site}>
+    <section className={Style.site__section}>
       <div className={Style.container}>
-        {/*<Carousel></Carousel>
-        <Carousel></Carousel>*/}
+        <h2 className={Style.site__section__title}>
+          РЕКОМЕНДУЕМЫЕ РЕСУРСЫ <br /> ДЛЯ УЛУЧШЕНИЯ УРОВНЯ АНГЛИЙСКОГО ЯЗЫКА
+        </h2>
+        <Carousel
+          arrows={false}
+          showDots={true}
+          responsive={responsive}
+          keyBoardControl={true}
+        >
+          <div>
+            <WebSiteItem />
+            <WebSiteItem />
+          </div>
+          <div>
+            <WebSiteItem />
+            <WebSiteItem />
+          </div>
+        </Carousel>
       </div>
     </section>
   );
