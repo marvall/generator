1385c7bce88fecb0a491fc7eadc3532b2e085179
Ihor Prokopets <kiringa23@gmail.com>
2021-04-26 11:03:26 +0300

    add some media

--- src/components/header/AppBar/AppBar.jsx
+++ src/components/header/AppBar/AppBar.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import Navigation from "../Navigation";
+import Navigation from '../Navigation';
 //import Container from "../../../Container";
 // import routes from "../../routes";
 
-import styles from "./AppBar.module.scss";
+import styles from './AppBar.module.scss';
 
-import defAva from "./defaultAvatar.png";
-import logo from "./Logo.png";
+import defAva from './defaultAvatar.png';
+import logo from './Logo.png';
+import menu from './menu.svg';
+import menuClose from './menuClose.svg';
 
 const AppBar = () => {
   return (
@@ -31,6 +33,16 @@ const AppBar = () => {
             height="50px"
             className={styles.ava}
           />
+          <button type="button" className={styles.menuButton}>
+            <img src={menu} alt="" width="28px" height="24px" />
+            <img
+              src={menuClose}
+              alt=""
+              width="33.33px"
+              height="33.33px"
+              className={styles.close}
+            />
+          </button>
         </div>
       </div>
     </header>
